f3ee4f9be20703e993ad9b48fadb97b811d7f70e
louyang <louyang367@gmail.com>
2018-05-23 14:19:34 -0700

    use sequalizer to persist contact object to postgres

---
+++ spec/contact_controller_spec.js
@@ -0,0 +1,33 @@
+const sequelize = require("../db/models/index").sequelize;
+const ContactController = require("../controllers/ContactController");
+
+describe("ContactController", () => {
+
+  beforeEach((done) => {
+    this.book = new ContactController();
+
+    sequelize.sync({force: true}).then((res) => {
+      done();
+    })
+    .catch((err) => {
+      done();
+    });
+  });
+
+  // #1
+  describe("#addContact()", () => {
+    it("should add a single contact into the book", (done) => {
+      this.book.addContact("Alice", "001-101-1010")
+      .then((contact) => {
+
+        // #3
+        expect(contact.name).toBe("Alice");
+        expect(contact.phone).toBe("001-101-1010");
+        done();
+      })
+      .catch((err) => {
+        done();
+      });
+    });
+  });
+})
