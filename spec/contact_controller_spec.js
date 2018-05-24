e737a1e748be3b4d7c42f3c05c5d97b0dd285974
louyang <louyang367@gmail.com>
2018-05-24 00:50:04 -0700

    added email to Contact object

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -17,12 +17,13 @@ describe("ContactController", () => {
   // #1
   describe("#addContact()", () => {
     it("should add a single contact into the book", (done) => {
-      this.book.addContact("Alice", "001-101-1010")
+      this.book.addContact("Alice", "001-101-1010", "alice@gmail.com")
       .then((contact) => {
 
         // #3
         expect(contact.name).toBe("Alice");
         expect(contact.phone).toBe("001-101-1010");
+        expect(contact.email).toBe("alice@gmail.com");
         done();
       })
       .catch((err) => {
